ff1de774e66181119cb1cec443a611265a360307
Abhijith <abhijithm050@gmail.com>
2024-01-15 19:28:28 +0530

    final1

--- middleware/auth.js
+++ middleware/auth.js
@@ -15,10 +15,29 @@ const User = require('../models/userModel')
         
 //     }
 // }
+// const isLogin = async (req, res, next) => {
+//     try {
+//         if (req.session.userId) {
+//             next();
+//         } else {
+//             res.redirect('/login');
+//         }
+//     } catch (error) {
+//         console.error('Error in isLogin middleware:', error);
+//         res.status(500).json({ error: 'Internal Server Error' });
+//     }
+// }
+
 const isLogin = async (req, res, next) => {
     try {
         if (req.session.userId) {
-            next();
+            const user = await User.findById(req.session.userId);
+
+            if (user && user.is_active) {
+                next();
+            } else {
+                res.redirect('/login');
+            }
         } else {
             res.redirect('/login');
         }
@@ -26,8 +45,7 @@ const isLogin = async (req, res, next) => {
         console.error('Error in isLogin middleware:', error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
-}
-
+};
 
 
 const isLogout = async (req, res, next) => {
